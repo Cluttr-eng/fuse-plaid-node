0906282997b2e508809e43bf3541054856d0974b
Tunde Alao <46252854+TundeAlao@users.noreply.github.com>
2023-02-23 16:59:34 +0000

    WIP

--- src/PlaidApi.ts
+++ src/PlaidApi.ts
@@ -1,4 +1,6 @@
 import {
+  AccountSubtype,
+  AccountType,
   Configuration,
   CountryCode,
   ItemPublicTokenExchangeRequest,
@@ -8,12 +10,16 @@ import {
   PlaidEnvironments
 } from "plaid";
 import {
+  AccountSubType as FuseAccountSubType,
+  AccountType as FuseAccountType,
   CreateLinkTokenRequest,
   CreateSessionRequest,
   ExchangeFinancialConnectionsPublicTokenRequest,
-  FuseApi, GetFinancialConnectionsAccountDetailsRequest, GetFinancialConnectionsAccountsResponse,
+  FuseApi,
+  GetFinancialConnectionsAccountDetailsRequest
 } from "fuse-node";
 import {AxiosResponse} from "axios";
+import {InvestmentAccountSubtype} from "plaid/api";
 
 export { CountryCode };
 export { Configuration }
@@ -37,20 +43,6 @@ export interface AccountBalance {
   iso_currency_code: string | null;
   unofficial_currency_code: string | null;
   last_updated_datetime?: string | null;
-}
-
-export enum AccountType {
-  Investment = 'investment',
-  Credit = 'credit',
-  Depository = 'depository',
-  Loan = 'loan',
-  Brokerage = 'brokerage',
-  Other = 'other'
-}
-
-export enum AccountSubtype {
-  Checking = 'checking',
-  Savings = 'savings',
 }
 
 export interface AccountBase {
@@ -103,6 +95,175 @@ export interface AuthGetResponse {
   request_id: string;
 }
 
+const fuseAccountTypeToPlaidType = (fuseAccountType: FuseAccountType): AccountType => {
+  switch (fuseAccountType) {
+    case FuseAccountType.Credit:
+      return AccountType.Credit
+    case FuseAccountType.Depository:
+      return AccountType.Depository
+    case FuseAccountType.Insurance:
+      return AccountType.Other
+    case FuseAccountType.Property:
+      return AccountType.Other
+    case FuseAccountType.Investment:
+      return AccountType.Investment
+    case FuseAccountType.Loan:
+      return AccountType.Loan;
+    case FuseAccountType.Other:
+      return AccountType.Other;
+  }
+}
+
+const fuseAccountSubtypeToPlaidSubtype = (fuseAccountSubtype: FuseAccountSubType): AccountSubtype | InvestmentAccountSubtype => {
+  switch (fuseAccountSubtype) {
+    case FuseAccountSubType.Checking:
+      return AccountSubtype.Checking;
+    case FuseAccountSubType.Savings:
+      return AccountSubtype.Savings;
+    case FuseAccountSubType.Hsa:
+      return AccountSubtype.Hsa;
+    case FuseAccountSubType.CertificateOfDeposit:
+      return AccountSubtype.Cd;
+    case FuseAccountSubType.MoneyMarket:
+      return AccountSubtype.MoneyMarket;
+    case FuseAccountSubType.Paypal:
+      return AccountSubtype.Paypal;
+    case FuseAccountSubType.Prepaid:
+      return AccountSubtype.Prepaid;
+    case FuseAccountSubType.CashManagement:
+      return AccountSubtype.CashManagement;
+    case FuseAccountSubType.Ebt:
+      return AccountSubtype.Ebt;
+    case FuseAccountSubType.CreditCard:
+      return AccountSubtype.CreditCard;
+    case FuseAccountSubType.Auto:
+      return AccountSubtype.Auto;
+    case FuseAccountSubType.Business:
+      return AccountSubtype.Business;
+    case FuseAccountSubType.Commercial:
+      return AccountSubtype.Commercial;
+    case FuseAccountSubType.Construction:
+      return AccountSubtype.Construction;
+    case FuseAccountSubType.Consumer:
+      return AccountSubtype.Consumer;
+    case FuseAccountSubType.HomeEquity:
+      return AccountSubtype.HomeEquity;
+    case FuseAccountSubType.Loan:
+      return AccountSubtype.Loan;
+    case FuseAccountSubType.Mortgage:
+      return AccountSubtype.Mortgage;
+    case FuseAccountSubType.Overdraft:
+      return AccountSubtype.Overdraft;
+    case FuseAccountSubType.LineOfCredit:
+      return AccountSubtype.LineOfCredit;
+    case FuseAccountSubType.Student:
+      return AccountSubtype.Student;
+    case FuseAccountSubType._529:
+      return AccountSubtype._529;
+    case FuseAccountSubType._401A:
+      return AccountSubtype._401a;
+    case FuseAccountSubType._401K:
+      return AccountSubtype._401k;
+    case FuseAccountSubType._403B:
+      return AccountSubtype._403B;
+    case FuseAccountSubType._457B:
+      return AccountSubtype._457b;
+    case FuseAccountSubType.Brokerage:
+      return AccountSubtype.Brokerage;
+    case FuseAccountSubType.CashIsa:
+      return AccountSubtype.CashIsa;
+    case FuseAccountSubType.CryptoExchange:
+      return AccountSubtype.CryptoExchange;
+    case FuseAccountSubType.EducationSavingAccount:
+      return AccountSubtype.EducationSavingsAccount;
+    case FuseAccountSubType.FixedAnnuity:
+      return AccountSubtype.FixedAnnuity;
+    case FuseAccountSubType.Gic:
+      return AccountSubtype.Gic;
+    case FuseAccountSubType.HealthReimbursementArrangement:
+      return AccountSubtype.HealthReimbursementArrangement;
+    case FuseAccountSubType.Ira:
+      return AccountSubtype.Ira;
+    case FuseAccountSubType.Isa:
+      return AccountSubtype.Isa;
+    case FuseAccountSubType.Keogh:
+      return AccountSubtype.Keogh;
+    case FuseAccountSubType.Lif:
+      return AccountSubtype.Lif;
+    case FuseAccountSubType.LifeInsurance:
+      return AccountSubtype.LifeInsurance;
+    case FuseAccountSubType.Lira:
+      return AccountSubtype.Lira;
+    case FuseAccountSubType.Lrif:
+      return AccountSubtype.Lrif;
+    case FuseAccountSubType.Lrsp:
+      return AccountSubtype.Lrsp;
+    case FuseAccountSubType.MutualFund:
+      return AccountSubtype.MutualFund;
+    case FuseAccountSubType.NonCustodialWallet:
+      return AccountSubtype.NonCustodialWallet;
+    case FuseAccountSubType.NonTaxableBrokerageAccount:
+      return AccountSubtype.NonTaxableBrokerageAccount;
+    case FuseAccountSubType.OtherAnnuity:
+      return AccountSubtype.OtherAnnuity;
+    case FuseAccountSubType.OtherInsurance:
+      return AccountSubtype.OtherInsurance;
+    case FuseAccountSubType.Pension:
+      return AccountSubtype.Pension;
+    case FuseAccountSubType.Prif:
+      return AccountSubtype.Prif;
+    case FuseAccountSubType.ProfitSharingPlan:
+      return AccountSubtype.ProfitSharingPlan;
+    case FuseAccountSubType.Qshr:
+      return InvestmentAccountSubtype.Qshr;
+    case FuseAccountSubType.Rdsp:
+      return AccountSubtype.Rdsp;
+    case FuseAccountSubType.Resp:
+      return AccountSubtype.Resp;
+    case FuseAccountSubType.Retirement:
+      return AccountSubtype.Retirement;
+    case FuseAccountSubType.Rlif:
+      return AccountSubtype.Rlif;
+    case FuseAccountSubType.RothIra:
+      return AccountSubtype.Roth;
+    case FuseAccountSubType.Roth401K:
+      return AccountSubtype.Roth401k;
+    case FuseAccountSubType.Rrif:
+      return AccountSubtype.Rrif;
+    case FuseAccountSubType.Rrsp:
+      return AccountSubtype.Rrsp;
+    case FuseAccountSubType.Sarsep:
+      return AccountSubtype.Sarsep;
+    case FuseAccountSubType.SepIra:
+      return AccountSubtype.SepIra;
+    case FuseAccountSubType.SimpleIra:
+      return AccountSubtype.SimpleIra;
+    case FuseAccountSubType.Sipp:
+      return AccountSubtype.Sipp;
+    case FuseAccountSubType.StockPlan:
+      return AccountSubtype.StockPlan;
+    case FuseAccountSubType.Tfsa:
+      return AccountSubtype.Tfsa;
+    case FuseAccountSubType.Trust:
+      return AccountSubtype.Trust;
+    case FuseAccountSubType.Ugma:
+      return AccountSubtype.Ugma;
+    case FuseAccountSubType.Utma:
+      return AccountSubtype.Utma;
+    case FuseAccountSubType.VariableAnnuity:
+      return AccountSubtype.VariableAnnuity;
+    default:
+      return AccountSubtype.Other
+  }
+}
+
+export interface AccountsBalanceGetRequest {
+  access_token: string
+}
+
+export interface AccountsBalanceGetResponse {
+  access_token: string
+}
 export class PlaidApi {
   private fuseApi: FuseApi;
 
@@ -120,6 +281,7 @@ export class PlaidApi {
       baseOptions: configuration.baseOptions
     } as any);
   }
+
 
   public sessionCreate = async (
     createSessionRequest: CreateSessionRequest,
@@ -224,8 +386,8 @@ export class PlaidApi {
           } as AccountBalance,
           mask: curAccount.mask,
           name: curAccount.name,
-          type: curAccount.type,
-          subtype: curAccount.subtype
+          type: fuseAccountTypeToPlaidType(curAccount.type),
+          subtype: fuseAccountSubtypeToPlaidSubtype(curAccount.subtype),
         } as AccountBase
       }),
       request_id: response.data.request_id
@@ -239,28 +401,15 @@ export class PlaidApi {
     const response = await this.fuseApi.getFinancialConnectionsAccountDetails(<GetFinancialConnectionsAccountDetailsRequest>{
       access_token: authGetRequest.access_token
     }, options);
+    const accountsResponse = await this.accountsGet({
+      access_token: authGetRequest.access_token
+    } as AccountsGetRequest);
+
     return <AuthGetResponse>{
       item: {
         item_id: response.data.financial_connection.id,
         institution_id: response.data.financial_connection.institution_id
       },
-      accounts: response.data.account_details.map(curAccountDetails => {
-        return {
-          account_id: curAccountDetails.remote_id,
-          balances: {
-            available: Number(curAccount.balance.available),
-            current: curAccount.balance.current,
-            iso_currency_code: curAccount.balance.iso_currency_code,
-            last_updated_datetime: curAccount.balance.last_updated_date,
-            limit: null,
-            unofficial_currency_code: null
-          } as AccountBalance,
-          mask: curAccount.mask,
-          name: curAccount.name,
-          type: curAccount.type,
-          subtype: curAccount.subtype
-        } as AccountBase
-      }),
       request_id: response.data.request_id,
       numbers: {
         ach: [{
@@ -273,8 +422,40 @@ export class PlaidApi {
           account_id: response.data.account_details[0].remote_id,
           account: response.data.account_details[0].ach.account,
           sort_code: response.data.account_details[0].ach.bacs_routing,
-        } as NumbersBACS]
-      }
+        } as NumbersBACS],
+      },
+      accounts: accountsResponse.accounts
+    }
+  }
+
+  public accountsBalanceGet = async (
+      accountsBalanceGetRequest: AccountsBalanceGetRequest,
+      options?: any
+  ) => {
+    const response = await this.fuseApi.getFinancialConnectionsBalances(<GetFinancialConnectionsAccountDetailsRequest>{
+      access_token: accountsBalanceGetRequest.access_token
+    }, options);
+    const accountsResponse = await this.accountsGet({
+      access_token: accountsBalanceGetRequest.access_token
+    } as AccountsGetRequest);
+
+
+    return <AccountsGetResponse>{
+      item: accountsResponse.item,
+      accounts: accountsResponse.accounts.map(curAccount => {
+        const respectiveBalance = response.data.balances.find((curBalance) => curAccount.account_id === curBalance);
+        return {
+          ...curAccount,
+          balances: {
+            available: Number(respectiveBalance.available),
+            current: respectiveBalance.current,
+            iso_currency_code: respectiveBalance.iso_currency_code,
+            limit: null,
+            unofficial_currency_code: null
+          } as AccountBalance
+        }
+      }),
+      request_id: response.data.request_id
     }
   }
 }
